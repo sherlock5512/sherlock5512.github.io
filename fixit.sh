0a92d5ce321ad34159c6b29ca8b41c317a7a76ee
Robert Morrison <sherlock5512@gmail.com>
2021-02-26 02:19:10 +0000

    Minor cleanup

--- fixit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-## this script will eventually be used to hold sed commands that fix the odd formatting issues that 
-## sometimes come from wordgrinder i.e adding indentation to raw lines..
-
-## to fix raw lines maybe add a {RAWLINE} tag at the line start to sed for then delete anything
-## up to and including it
